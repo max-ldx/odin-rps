484b512432c75afdf0aa048032fa59fffd9ab1a6
max-ldx <242964282+max-ldx@users.noreply.github.com>
2025-12-17 13:18:31 +0100

    fix: choice generation bounds

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const scores = {
 
 function getComputerChoice() {
     const rand = Math.random();
-    if (rand <= 1 / 3) {
+    if (rand < 1 / 3) {
         return choices.rock;
-    } else if (rand <= 2 / 3) {
+    } else if (rand < 2 / 3) {
         return choices.paper
     } else {
         return choices.scissors;
