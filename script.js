93c10947646d671046d487e38433ffc3a2cb8e5f
max-ldx <242964282+max-ldx@users.noreply.github.com>
2025-12-17 13:17:40 +0100

    feat: add logic to play a single round

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const choices = {
     scissors: 'scissors'
 };
 
+const scores = {
+    humanScore: 0,
+    computerScore: 0
+};
+
 function getComputerChoice() {
     const rand = Math.random();
     if (rand <= 1 / 3) {
@@ -16,9 +21,30 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const input = prompt('rock, paper or scissors?').toLowerCase();
-    if (input === choices.rock || input === choices.paper || input === choices.scissors) {
-        return input;
+    return prompt('rock, paper or scissors?').toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log('Tie!');
+    } else if (humanChoice === choices.rock) {
+        if (computerChoice === choices.scissors) {
+            console.log('You win! Rock beats scissors');
+        } else {
+            console.log('You lose! Paper beats rock');
+        }
+    } else if (humanChoice === choices.paper) {
+        if (computerChoice === choices.rock) {
+            console.log('You win! Paper beats rock');
+        } else {
+            console.log('You lose! Scissors beats paper');
+        }
+    } else {
+        if (computerChoice === choices.paper) {
+            console.log('You win! Scissors beats paper');
+        } else {
+            console.log('You lose! Rock beats scissors');
+        }
     }
-    return null;
 }
